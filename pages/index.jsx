1547cafb6ea111cefdbe7d350e1082ada099618c
BeratKaraaslanS <103049743+BeratKaraaslanS@users.noreply.github.com>
2024-07-02 15:26:08 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -83,6 +83,106 @@ Premium'a Yükselt!</a>
 
 
 
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Index() {
+	
+  return (
+    <>
+      <div className="py-20 mb-30 max-w-3xl mx-auto">
+        <div className="flex w-full justify-center">
+      <img width="500" />
+      </div>
+        <p className="animateHeader text-4xl font-bold text-center text-white">
+          Discord Deneyiminizi Üst Düzeye Çıkaracak Tek Bot Proxy
+        </p>
+        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
+       Proxy botunu kullanarak, sunucunuza renk katın. Güncellemeler ile eski kalmayan botumuzu hemen deneyin! 
+	  </p>
+        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
+  
+            <Link href={"https://discord.com/oauth2/authorize?client_id=1256640436085330041&permissions=8&integration_type=0&scope=applications.commands+bot"}>
+              <a
+                className={
+                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
+                }
+              >
+              <i className="fab fa-discord mr-2" />Discord'a ekle
+              </a>
+            </Link>
+  <div className="py-10"></div>
+  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
+Premium'a Yükselt!</a>
+  </Link>
+        </div>
+
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
+        <Image width="600" height="600"  />
+        </div>
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
+        <Image  width="600" height="600" />
+        </div>
+      </div>
+
+
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+          <div className="col-span-4">
+            <p className="text-white text-4xl font-medium" dangerouslySetInnerHTML={{ __html:"ProxyBot" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
+            <a href="https://discord.com/oauth2/authorize?client_id=1256640436085330041&permissions=8&integration_type=0&scope=applications.commands+bot" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Discord'a ekle
+            </a>
+          </div>
+
+          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+           
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
+                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, ProxyBot gives you a musical pleasure to the fullest.</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
+                  <p className="text-gray-500 line-clamp-4">A bot that supports multi-platform music playback. (Spotify, Souncloud, YouTube) | (It is a bot that complies with all copyrights and licenses.)</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
+                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop.</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Support</p>
+                  <p className="text-gray-500 line-clamp-4">With our best teammates, we provide the fastest response to anyone who needs help.</p>
+                </div>
+              </div>
+          </div>
+      </div>
+
+
+
+      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg">
+              <>
+                <div className="lg:mx-4 lg:p-8 py-4 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl" />
+                  <p className="text-3xl text-white font-semibold">👑 Sizin için en iyisi</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel bot olan ProxyBot ile en iyi partileri vermeye hazır mısınız?</p>
+                </div>
+                <img src="https://media.discordapp.net/attachments/1256645729968066593/1257673611238445169/reaction-role_2.png?ex=66854353&is=6683f1d3&hm=7c4cecdf31663b0e4f0de7e2b59013b1446dfafa3bf640359ebac2ce2f4286f1&=&format=webp&quality=lossless&width=871&height=671" className="" />
+              </>
+      
+        </div>
+
+      </>
+  )
+}
 
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
