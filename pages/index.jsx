ed2fdc3ef059d20ba659d0366f404d78d46b1034
BeratKaraaslanS <103049743+BeratKaraaslanS@users.noreply.github.com>
2024-07-02 15:23:43 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -83,7 +83,7 @@ Premium'a Yükselt!</a>
 
 
 
-      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg">
+
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl" />
